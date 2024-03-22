a557fbc638518083967b071c44b0523884963d39
Dimas Mulya <64884902+dimasmuly@users.noreply.github.com>
2024-03-22 17:19:43 +0700

    Update processor.py

--- modules/processor.py
+++ modules/processor.py
@@ -1,21 +1,22 @@
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, AutoModel
 import torch
 import nltk
 from nltk.corpus import stopwords
-from transformers import AutoModel
 
 access_token = "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"
 
+# Pemrosesan Data
+tokenizer = AutoTokenizer.from_pretrained("guriko/autotrain-cv_resume-56492130967", token=access_token)
+model_sequence = AutoModelForSequenceClassification.from_pretrained("guriko/autotrain-cv_resume-56492130967", token=access_token)
 model = AutoModel.from_pretrained("guriko/autotrain-cv_resume-56492130967", token=access_token)
 
-# Pemrosesan Data
-tokenizer = AutoTokenizer.from_pretrained("guriko/autotrain-cv_resume-56492130967", use_auth_token=True)
-model = AutoModelForSequenceClassification.from_pretrained("guriko/autotrain-cv_resume-56492130967", use_auth_token=True)
 nltk.download('stopwords')
 stop_words = set(stopwords.words('indonesian'))
 
 def process_text(text):
     text = ' '.join([word for word in text.split() if word.lower() not in stop_words])
     inputs = tokenizer(text, return_tensors="pt")
-    outputs = model(**inputs)
+    outputs = model_sequence(**inputs)
     return torch.nn.functional.softmax(outputs.logits, dim=-1)
+
+
